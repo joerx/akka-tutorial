7d7489c29838e009dbaa9d38a394a327241132bf
Joerg Henning <jhenning@vostros.(none)>
2012-11-26 15:33:33 +0700

    Using Futures for result retrieval now

--- src/main/java/home/jhenning/akkapi/Pi.java
+++ src/main/java/home/jhenning/akkapi/Pi.java
@@ -1,20 +1,29 @@
 package home.jhenning.akkapi;
 
+import static akka.pattern.Patterns.ask;
+
 import akka.actor.*;
-import home.jhenning.akkapi.actors.Listener;
+import akka.dispatch.Await;
+import akka.dispatch.Future;
+import akka.util.Duration;
+import akka.util.Timeout;
 import home.jhenning.akkapi.actors.Master;
 import home.jhenning.akkapi.messages.Calculate;
+import home.jhenning.akkapi.messages.PiApproximation;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
- * Created with IntelliJ IDEA.
- * User: jhenning
- * Date: 11/26/12
- * Time: 1:57 PM
- * To change this template use File | Settings | File Templates.
+ * Actor based PI calculation using Akka. Main class, starts the calculation of PI. Based on the getting started guide
+ * on the Typesafe website (http://typesafe.com/resources/tutorials/getting-started-with-akka-java.html).
+ *
+ * This version has been improved to use Futures instead of direct Actor-messages to return the result. One of the
+ * advantages of this approach is, that the Master worker is now stateless.
  */
 public class Pi {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Pi pi = new Pi();
     pi.calculate(4, 10000, 10000);
   }
@@ -22,18 +31,25 @@ public class Pi {
   public void calculate(
       final int nrOfWorkers,
       final int nrOfElements,
-      final int nrOfMessages)
-  {
-    ActorSystem system = ActorSystem.create("PiSystem");
-    final ActorRef listener = system.actorOf(new Props(Listener.class), "listener");
-
+      final int nrOfMessages) throws Exception {
+    final ActorSystem system = ActorSystem.create("PiSystem");
     final ActorRef master = system.actorOf(new Props(new UntypedActorFactory() {
       @Override
       public Actor create() throws Exception {
-        return new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener);
+        return new Master(nrOfWorkers, nrOfMessages, nrOfElements);
       }
     }), "master");
 
-    master.tell(new Calculate());
+    Timeout t = new Timeout(Duration.create(1, TimeUnit.SECONDS));
+    Future<Object> f = ask(master, new Calculate(), t);
+    PiApproximation approx = (PiApproximation) Await.result(f, t.duration());
+
+    System.out.println(String.format("\n\tPI Approximation: " +
+        "\t\t%s\n\tCalculation time: \t%s",
+        approx.getPi(),
+        approx.getDuration()
+    ));
+
+    system.shutdown();
   }
 }
--- src/main/java/home/jhenning/akkapi/actors/Master.java
+++ src/main/java/home/jhenning/akkapi/actors/Master.java
@@ -1,15 +1,24 @@
 package home.jhenning.akkapi.actors;
 
+import static akka.pattern.Patterns.ask;
+
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import akka.dispatch.Future;
+import akka.dispatch.Futures;
+import akka.dispatch.Mapper;
+import akka.dispatch.OnSuccess;
 import akka.routing.RoundRobinRouter;
 import akka.util.Duration;
+import akka.util.Timeout;
 import home.jhenning.akkapi.messages.Calculate;
 import home.jhenning.akkapi.messages.PiApproximation;
 import home.jhenning.akkapi.messages.Result;
 import home.jhenning.akkapi.messages.Work;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,44 +32,57 @@ public class Master extends UntypedActor {
 
   private final int nrOfMessages;
   private final int nrOfElements;
-
-  private double pi;
-  private int nrOfResults;
   private final long start = System.currentTimeMillis();
 
-  private final ActorRef listener;
   private final ActorRef workerRouter;
+
+  private final List<Future<Object>> listOfFutureResults;
 
   public Master(
       final int nrOfWorkers,
       int nrOfMessages,
-      int nrOfElements,
-      ActorRef listener)
+      int nrOfElements)
   {
     this.nrOfMessages = nrOfMessages;
     this.nrOfElements = nrOfElements;
-    this.listener = listener;
 
     workerRouter = this.getContext().actorOf(new Props(Worker.class).withRouter(
         new RoundRobinRouter(nrOfWorkers)), "workerRouter");
+
+    listOfFutureResults = new ArrayList<Future<Object>>(nrOfElements);
   }
 
 
   @Override
   public void onReceive(Object message) throws Exception {
     if (message instanceof Calculate) {
+
+      final ActorRef sender = getSender();
+
       for (int start = 0; start < nrOfMessages; start ++) {
-        workerRouter.tell(new Work(start, nrOfElements), getSelf());
+        listOfFutureResults.add(
+            ask(workerRouter, new Work(start, nrOfElements), new Timeout(Duration.create(5, TimeUnit.SECONDS)))
+        );
       }
-    } else if (message instanceof Result) {
-      Result result = (Result) message;
-      pi += result.getValue();
-      nrOfResults += 1;
-      if (nrOfResults == nrOfMessages) {
-        Duration duration = Duration.create(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
-        listener.tell(new PiApproximation(pi, duration), getSelf());
-        getContext().stop(getSelf());
-      }
+      Futures.sequence(listOfFutureResults, getContext().dispatcher()).map(new Mapper<Iterable<Object>, PiApproximation>()
+      {
+        @Override
+        public PiApproximation apply(Iterable<Object> parameter) {
+          double pi = 0d;
+          for (Object o: parameter) {
+            Result r = (Result) o;
+            pi += r.getValue();
+          }
+          return new PiApproximation(pi, Duration.create(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS));
+        }
+      }).onSuccess(new OnSuccess<PiApproximation>()
+      {
+        @Override
+        public void onSuccess(PiApproximation piApproximation) throws Throwable {
+          sender.tell(piApproximation);
+          getContext().stop(getSelf());
+        }
+      });
     } else {
       unhandled(message);
     }
